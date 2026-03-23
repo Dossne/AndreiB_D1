edfa6c829dddd359fff5e5c3551361cf500ee73e
Andrei Brui <a.brui@whalergames.com>
2026-03-23 17:16:22 +0500

    Add generated snake head body tail sprites

--- Assets/Scripts/SnakeGameController.cs
+++ Assets/Scripts/SnakeGameController.cs
@@ -37,6 +37,9 @@ public class SnakeGameController : MonoBehaviour
     private readonly List<string> mazeRows = new();
 
     private static Sprite cachedSquareSprite;
+    private static Sprite cachedSnakeHeadSprite;
+    private static Sprite cachedSnakeBodySprite;
+    private static Sprite cachedSnakeTailSprite;
 
     private Transform boardRoot;
     private Transform dotsRoot;
@@ -333,8 +336,11 @@ public class SnakeGameController : MonoBehaviour
         for (var i = 0; i < snakeSegments.Count; i++)
         {
             var view = segmentViews[i];
+            var renderer = view.GetComponent<SpriteRenderer>();
             view.name = $"Segment_{i}";
-            view.GetComponent<SpriteRenderer>().color = i == 0 ? new Color(0.43f, 0.9f, 0.5f) : new Color(0.26f, 0.72f, 0.34f);
+            renderer.color = Color.white;
+            renderer.sprite = GetSnakeSegmentSprite(i);
+            view.rotation = GetSnakeSegmentRotation(i);
         }
     }
 
@@ -373,7 +379,7 @@ public class SnakeGameController : MonoBehaviour
 
             var view = CreateCell(
                 snakeSegments[Mathf.Min(segmentViews.Count, snakeSegments.Count - 1)],
-                segmentViews.Count == 0 ? new Color(0.43f, 0.9f, 0.5f) : new Color(0.26f, 0.72f, 0.34f),
+                Color.white,
                 $"Segment_{segmentViews.Count}",
                 snakeRoot,
                 new Vector3(0.84f, 0.84f, 1f)).transform;
@@ -501,6 +507,153 @@ public class SnakeGameController : MonoBehaviour
         }
 
         return cachedSquareSprite;
+    }
+
+    private Sprite GetSnakeSegmentSprite(int index)
+    {
+        if (index == 0)
+        {
+            return CreateSnakeHeadSprite();
+        }
+
+        if (index == snakeSegments.Count - 1)
+        {
+            return CreateSnakeTailSprite();
+        }
+
+        return CreateSnakeBodySprite();
+    }
+
+    private Quaternion GetSnakeSegmentRotation(int index)
+    {
+        Vector2Int direction;
+
+        if (index == 0)
+        {
+            if (snakeSegments.Count == 1)
+            {
+                direction = snakeDirection;
+            }
+            else
+            {
+                direction = snakeSegments[0] - snakeSegments[1];
+            }
+        }
+        else if (index == snakeSegments.Count - 1)
+        {
+            direction = snakeSegments[snakeSegments.Count - 2] - snakeSegments[snakeSegments.Count - 1];
+        }
+        else
+        {
+            direction = snakeSegments[index - 1] - snakeSegments[index + 1];
+        }
+
+        return Quaternion.Euler(0f, 0f, DirectionToAngle(direction));
+    }
+
+    private static float DirectionToAngle(Vector2Int direction)
+    {
+        if (direction == Vector2Int.up)
+        {
+            return 90f;
+        }
+
+        if (direction == Vector2Int.left)
+        {
+            return 180f;
+        }
+
+        if (direction == Vector2Int.down)
+        {
+            return 270f;
+        }
+
+        return 0f;
+    }
+
+    private static Sprite CreateSnakeHeadSprite()
+    {
+        if (cachedSnakeHeadSprite == null)
+        {
+            cachedSnakeHeadSprite = CreateSnakeSprite(texture =>
+            {
+                var bodyColor = new Color32(110, 215, 90, 255);
+                var eyeColor = new Color32(28, 33, 35, 255);
+                var tongueColor = new Color32(250, 95, 95, 255);
+
+                FillRect(texture, 2, 2, 12, 12, bodyColor);
+                FillRect(texture, 10, 5, 3, 2, eyeColor);
+                FillRect(texture, 10, 9, 3, 2, eyeColor);
+                FillRect(texture, 13, 7, 2, 2, tongueColor);
+            });
+        }
+
+        return cachedSnakeHeadSprite;
+    }
+
+    private static Sprite CreateSnakeBodySprite()
+    {
+        if (cachedSnakeBodySprite == null)
+        {
+            cachedSnakeBodySprite = CreateSnakeSprite(texture =>
+            {
+                var bodyColor = new Color32(78, 176, 70, 255);
+                var stripeColor = new Color32(128, 224, 112, 255);
+
+                FillRect(texture, 2, 2, 12, 12, bodyColor);
+                FillRect(texture, 5, 4, 6, 8, stripeColor);
+            });
+        }
+
+        return cachedSnakeBodySprite;
+    }
+
+    private static Sprite CreateSnakeTailSprite()
+    {
+        if (cachedSnakeTailSprite == null)
+        {
+            cachedSnakeTailSprite = CreateSnakeSprite(texture =>
+            {
+                var bodyColor = new Color32(62, 152, 56, 255);
+
+                FillRect(texture, 2, 4, 8, 8, bodyColor);
+                FillRect(texture, 9, 5, 3, 6, bodyColor);
+                FillRect(texture, 12, 6, 2, 4, bodyColor);
+            });
+        }
+
+        return cachedSnakeTailSprite;
+    }
+
+    private static Sprite CreateSnakeSprite(System.Action<Texture2D> painter)
+    {
+        var texture = new Texture2D(16, 16, TextureFormat.RGBA32, false);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
+
+        var transparent = new Color32(0, 0, 0, 0);
+        var pixels = new Color32[16 * 16];
+        for (var i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = transparent;
+        }
+
+        texture.SetPixels32(pixels);
+        painter(texture);
+        texture.Apply();
+
+        return Sprite.Create(texture, new Rect(0f, 0f, 16f, 16f), new Vector2(0.5f, 0.5f), 16f);
+    }
+
+    private static void FillRect(Texture2D texture, int startX, int startY, int width, int height, Color32 color)
+    {
+        for (var x = startX; x < startX + width; x++)
+        {
+            for (var y = startY; y < startY + height; y++)
+            {
+                texture.SetPixel(x, y, color);
+            }
+        }
     }
 
     private void BuildExpandedMaze()
