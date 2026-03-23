e43d64e3fe27c8fa10b913f97863fb94db631f6e
Andrei Brui <a.brui@whalergames.com>
2026-03-23 18:21:38 +0500

    Fix snake segment interpolation

--- Assets/Scripts/SnakeGameController.cs
+++ Assets/Scripts/SnakeGameController.cs
@@ -377,6 +377,7 @@ public class SnakeGameController : MonoBehaviour
             view.name = $"Segment_{i}";
             renderer.color = Color.white;
             renderer.sprite = GetSnakeSegmentSprite(i);
+            renderer.sortingOrder = 6;
             view.rotation = GetSnakeSegmentRotation(i);
         }
     }
@@ -386,9 +387,9 @@ public class SnakeGameController : MonoBehaviour
         var snakeLerp = Mathf.Clamp01(moveTimer / MoveInterval);
         for (var i = 0; i < snakeSegments.Count && i < segmentViews.Count; i++)
         {
-            var fromGrid = i == 0
-                ? previousSnakeSegments[0]
-                : previousSnakeSegments[Mathf.Min(i - 1, previousSnakeSegments.Count - 1)];
+            var fromGrid = i < previousSnakeSegments.Count
+                ? previousSnakeSegments[i]
+                : previousSnakeSegments[previousSnakeSegments.Count - 1];
             var toGrid = snakeSegments[i];
             segmentViews[i].position = Vector3.Lerp(
                 GridToWorld(fromGrid),
